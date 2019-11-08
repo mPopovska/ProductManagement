a5132b9af8229730f66f397d19a3bd16cb85e069
mpopovska <mateapopovska@gmail.com>
2019-11-08 21:26:45 +0100

    product and customer models

--- ProductManagement/src/app/modules/customers/models/customerModels.ts
+++ ProductManagement/src/app/modules/customers/models/customerModels.ts
@@ -2,7 +2,7 @@ import { ProductsModule } from '../../products/products.module';
 
 export class Customer {
     id: string;
-    name: string;
-    email: string;
-    purchasedProducts: Array<ProductsModule>;
+    name: string = "";
+    email: string = "";
+    purchasedProducts: Array<ProductsModule> = [];
 }
