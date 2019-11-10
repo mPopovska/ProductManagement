acd90430e56f05899637ac6c287d2fee60fdcf7d
mpopovska <mateapopovska@gmail.com>
2019-11-10 22:48:00 +0100

    Fixing heroku issue with server.js

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname+'/dist/ProductManagement'));
 
+app.listen(process.env.PORT || 8080);
+
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/ProductManagement/index.html'));
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+console.log("PAtekata:",__dirname, path.dirname("./"));
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
