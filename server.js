7ae15c5b09c20cc1e0538c415baa6b123947980c
mpopovska <mateapopovska@gmail.com>
2019-11-10 20:28:02 +0100

    fixing problem with heroku path

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 
 const app = express();
 
-fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        console.log(file); 
-    });
-});
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(__dirname+'/dist/ProductManagement'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ProductManagement/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
