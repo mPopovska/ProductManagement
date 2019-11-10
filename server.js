c2994dbf5eaf18df41bf58ad10201a17fe3ede9d
mpopovska <mateapopovska@gmail.com>
2019-11-10 20:16:37 +0100

    log content of directory for path fix

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
+
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file); 
+    });
+});
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname+'/dist'));
